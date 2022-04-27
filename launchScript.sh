04e1bf24c6f5bd4f5c9cd414defa7861630afa21
Fred Smith Jr <frsmith@squarespace.com>
2022-04-27 15:30:33 +0100

    tweaking clone9

--- launchScript.sh
+++ launchScript.sh
@@ -1,20 +1,33 @@
 #! /usr/bin/env bash
 # Bail from script if any command fails
-set -e
+set -xe
 localenvpath=".localenv"
 . ./.instantiate.sh ${localenvpath}
 . ./${localenvpath} # source the localenv vile created by instantiate
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 DEFAULTUSER="shiftedmr"
-DRONESERVNAME="localdrone"
-DRONERUNNAME="local-drone-runner-1"
+DRONESERVNAME="localdrone.local"
+DRONERUNNAME="local-drone-runner-1.local"
 DOCKERNETWORK="localdronenet"
-GITEASRVNAME="localgitea"
+GITEASRVNAME="localgitea.local"
 dserverrunning=$(docker ps -f "name=${DRONESERVNAME}" --format '{{.Names}}'|wc -l)
 drunrunning=$(docker ps -f "name=${DRONERUNNAME}" --format '{{.Names}}'|wc -l)
 dnetExist=$(docker network list -f name=${DOCKERNETWORK} --format "{{.Name}}"|wc -l)
 gitearunning=$(docker ps -f "name=${GITEASRVNAME}" --format '{{.Names}}'|wc -l)
 #Docker bridge network
+
+if [[ "$#" -gt "0" ]] && [[ "$1" -eq "stop" ]]
+then
+  docker ps -f "name=${DRONESERVNAME}" --format '{{.Names}}' && docker stop ${DRONESERVNAME} && docker rm ${DRONESERVNAME}
+  docker ps -f "name=${DRONERUNNAME}" --format '{{.Names}}' && docker stop ${DRONERUNNAME} && docker rm ${DRONERUNNAME}
+  docker ps -f "name=${GITEASRVNAME}" --format '{{.Names}}' && docker stop ${GITEASRVNAME} && docker rm ${GITEASRVNAME}
+  if [[ "$2" -eq "vols" ]]
+  then
+    [ -d "${SCRIPT_DIR}/volumes/giteavol" ] && rm -rf "${SCRIPT_DIR}/volumes/giteavol"
+    [ -d "${SCRIPT_DIR}/volumes/drone" ] && rm -rf "${SCRIPT_DIR}/volumes/drone"
+  fi
+  exit 0
+fi
 
 [ -d "${SCRIPT_DIR}/volumes" ] || mkdir "${SCRIPT_DIR}/volumes"
 
@@ -44,12 +57,12 @@ then
     fredtest:latest 
   echo "Creating gitea admin"
   sleep 20
-  docker exec -u git -i localgitea sh -c "gitea admin user create --username ${DEFAULTUSER} --password supersecret --email haha@no.com --access-token --must-change-password=false" &> output
+  docker exec -u git -i ${GITEASRVNAME} sh -c "gitea admin user create --username ${DEFAULTUSER} --password supersecret --email haha@no.com --admin --access-token --must-change-password=false" | tee output
   rslt="$(cat output)"
   echo "rslt: ${rslt}"
   admin_user_token=$(echo "${rslt}"|grep "Access token" | awk -F "created... " '{print $2}')
   echo "creating drone oauth app in gitea"
-  jsonbody='{"Name":"localdrone","redirect_uris":["http://--DRONE--/login"]}'
+  jsonbody='{"Name":"localdroneapp","redirect_uris":["http://--DRONE--/login"]}'
   oauth_app_json_blob=$(curl -d"${jsonbody/--DRONE--/${DRONESERVNAME}}" -H"Content-type: application/json"  -H "Authorization: Bearer ${admin_user_token}" ${GITEASRVNAME}:3001/api/v1/user/applications/oauth2)
   export DRONE_GITEA_CLIENT_ID=$(echo "${oauth_app_json_blob}" | jq '.client_id' | sed 's/"//g')
   export DRONE_GITEA_CLIENT_SECRET=$(echo "${oauth_app_json_blob}" | jq '.client_secret' | sed 's/"//g')
@@ -74,6 +87,7 @@ then
     --env=DRONE_GITEA_CLIENT_ID=${DRONE_GITEA_CLIENT_ID} \
     --env=DRONE_GITEA_CLIENT_SECRET=${DRONE_GITEA_CLIENT_SECRET} \
     --env=DRONE_GITEA_SERVER=http://${GITEASRVNAME}:3001 \
+    --env=DRONE_RPC_SECRET=${LOCAL_DRONE_SECRET} \
     --env=DRONE_SERVER_PROTO=${DRONEURIPROTO} \
     --env=DRONE_SERVER_HOST=${DRONESERVNAME} \
     --env=DRONE_RUNNER_CAPACITY=2 \
@@ -100,6 +114,7 @@ then
     --env=DRONE_RPC_SECRET=${LOCAL_DRONE_SECRET} \
     --env=DRONE_RUNNER_CAPACITY=2 \
     --env=DRONE_RUNNER_NAME=${DRONERUNNAME} \
+    --env=DRONE_RUNNER_NETWORKS="localdronenet" \
     --publish=3000:3000 \
     --restart=always \
     --name=${DRONERUNNAME} \
